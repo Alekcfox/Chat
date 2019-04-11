073491208159d346bcf09b2c9ef4aca2a126645a
Alekcfox <Alekcfox@yandex.ru>
2019-04-11 22:22:08 +0800

    1

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.ChatWindow = new System.Windows.Forms.RichTextBox();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.TestBoxL = new System.Windows.Forms.RichTextBox();
+            this.TestBoxS = new System.Windows.Forms.RichTextBox();
+            this.Privat = new System.Windows.Forms.GroupBox();
+            this.ButtonChat5 = new System.Windows.Forms.Button();
+            this.ButtonChat4 = new System.Windows.Forms.Button();
+            this.ButtonChat3 = new System.Windows.Forms.Button();
+            this.ButtonChat2 = new System.Windows.Forms.Button();
+            this.ButtonChat1 = new System.Windows.Forms.Button();
+            this.ButtonChat0 = new System.Windows.Forms.Button();
+            this.Name2Box = new System.Windows.Forms.TextBox();
+            this.CreatePrivatChatButton = new System.Windows.Forms.Button();
+            this.DeletehisChatButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.Privat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.BackColor = System.Drawing.Color.PowderBlue;
+            this.NameBox.ForeColor = System.Drawing.Color.Red;
+            this.NameBox.Location = new System.Drawing.Point(66, 12);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(218, 20);
+            this.NameBox.TabIndex = 0;
+            this.NameBox.Text = "Введите своё имя";
+            this.NameBox.Enter += new System.EventHandler(this.NameBox_Enter);
+            this.NameBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NameBox_KeyPress);
+            // 
+            // ChatWindow
+            // 
+            this.ChatWindow.BackColor = System.Drawing.Color.PowderBlue;
+            this.ChatWindow.Location = new System.Drawing.Point(12, 54);
+            this.ChatWindow.Name = "ChatWindow";
+            this.ChatWindow.Size = new System.Drawing.Size(347, 196);
+            this.ChatWindow.TabIndex = 1;
+            this.ChatWindow.Text = "Чат";
+            this.ChatWindow.TextChanged += new System.EventHandler(this.ChatWindow_TextChanged);
+            // 
+            // InputBox
+            // 
+            this.InputBox.BackColor = System.Drawing.Color.PowderBlue;
+            this.InputBox.ForeColor = System.Drawing.Color.Gray;
+            this.InputBox.Location = new System.Drawing.Point(12, 265);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(584, 20);
+            this.InputBox.TabIndex = 2;
+            this.InputBox.Text = "Введите ваше сообщение";
+            this.InputBox.Enter += new System.EventHandler(this.InputBox_Enter);
+            this.InputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputBox_KeyPress);
+            // 
+            // TestBoxL
+            // 
+            this.TestBoxL.Location = new System.Drawing.Point(638, 33);
+            this.TestBoxL.Name = "TestBoxL";
+            this.TestBoxL.Size = new System.Drawing.Size(159, 172);
+            this.TestBoxL.TabIndex = 4;
+            this.TestBoxL.Text = "Test1";
+            // 
+            // TestBoxS
+            // 
+            this.TestBoxS.Location = new System.Drawing.Point(638, 235);
+            this.TestBoxS.Name = "TestBoxS";
+            this.TestBoxS.Size = new System.Drawing.Size(171, 85);
+            this.TestBoxS.TabIndex = 5;
+            this.TestBoxS.Text = "Test2";
+            // 
+            // Privat
+            // 
+            this.Privat.Controls.Add(this.ButtonChat5);
+            this.Privat.Controls.Add(this.ButtonChat4);
+            this.Privat.Controls.Add(this.ButtonChat3);
+            this.Privat.Controls.Add(this.ButtonChat2);
+            this.Privat.Controls.Add(this.ButtonChat1);
+            this.Privat.Controls.Add(this.ButtonChat0);
+            this.Privat.Location = new System.Drawing.Point(365, 54);
+            this.Privat.Name = "Privat";
+            this.Privat.Size = new System.Drawing.Size(231, 196);
+            this.Privat.TabIndex = 6;
+            this.Privat.TabStop = false;
+            this.Privat.Text = "Privat";
+            // 
+            // ButtonChat5
+            // 
+            this.ButtonChat5.Location = new System.Drawing.Point(6, 164);
+            this.ButtonChat5.Name = "ButtonChat5";
+            this.ButtonChat5.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat5.TabIndex = 12;
+            this.ButtonChat5.UseVisualStyleBackColor = true;
+            this.ButtonChat5.Click += new System.EventHandler(this.ButtonChat5_Click);
+            // 
+            // ButtonChat4
+            // 
+            this.ButtonChat4.Location = new System.Drawing.Point(6, 135);
+            this.ButtonChat4.Name = "ButtonChat4";
+            this.ButtonChat4.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat4.TabIndex = 11;
+            this.ButtonChat4.UseVisualStyleBackColor = true;
+            this.ButtonChat4.Click += new System.EventHandler(this.ButtonChat4_Click);
+            // 
+            // ButtonChat3
+            // 
+            this.ButtonChat3.Location = new System.Drawing.Point(6, 106);
+            this.ButtonChat3.Name = "ButtonChat3";
+            this.ButtonChat3.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat3.TabIndex = 10;
+            this.ButtonChat3.UseVisualStyleBackColor = true;
+            this.ButtonChat3.Click += new System.EventHandler(this.ButtonChat3_Click);
+            // 
+            // ButtonChat2
+            // 
+            this.ButtonChat2.BackColor = System.Drawing.SystemColors.Control;
+            this.ButtonChat2.Location = new System.Drawing.Point(6, 77);
+            this.ButtonChat2.Name = "ButtonChat2";
+            this.ButtonChat2.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat2.TabIndex = 9;
+            this.ButtonChat2.UseVisualStyleBackColor = false;
+            this.ButtonChat2.Click += new System.EventHandler(this.ButtonChat2_Click);
+            // 
+            // ButtonChat1
+            // 
+            this.ButtonChat1.Location = new System.Drawing.Point(6, 48);
+            this.ButtonChat1.Name = "ButtonChat1";
+            this.ButtonChat1.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat1.TabIndex = 8;
+            this.ButtonChat1.UseVisualStyleBackColor = true;
+            this.ButtonChat1.Click += new System.EventHandler(this.ButtonChat1_Click);
+            // 
+            // ButtonChat0
+            // 
+            this.ButtonChat0.Location = new System.Drawing.Point(6, 19);
+            this.ButtonChat0.Name = "ButtonChat0";
+            this.ButtonChat0.Size = new System.Drawing.Size(215, 23);
+            this.ButtonChat0.TabIndex = 7;
+            this.ButtonChat0.UseVisualStyleBackColor = true;
+            this.ButtonChat0.Click += new System.EventHandler(this.ButtonChat0_Click);
+            // 
+            // Name2Box
+            // 
+            this.Name2Box.BackColor = System.Drawing.Color.PowderBlue;
+            this.Name2Box.Location = new System.Drawing.Point(445, 9);
+            this.Name2Box.Name = "Name2Box";
+            this.Name2Box.Size = new System.Drawing.Size(151, 20);
+            this.Name2Box.TabIndex = 7;
+            // 
+            // CreatePrivatChatButton
+            // 
+            this.CreatePrivatChatButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.CreatePrivatChatButton.Location = new System.Drawing.Point(365, 9);
+            this.CreatePrivatChatButton.Name = "CreatePrivatChatButton";
+            this.CreatePrivatChatButton.Size = new System.Drawing.Size(74, 23);
+            this.CreatePrivatChatButton.TabIndex = 8;
+            this.CreatePrivatChatButton.Text = "Connect to:";
+            this.CreatePrivatChatButton.UseVisualStyleBackColor = false;
+            this.CreatePrivatChatButton.Click += new System.EventHandler(this.CreatePrivatChatButton_Click);
+            // 
+            // DeletehisChatButton
+            // 
+            this.DeletehisChatButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.DeletehisChatButton.Location = new System.Drawing.Point(504, 35);
+            this.DeletehisChatButton.Name = "DeletehisChatButton";
+            this.DeletehisChatButton.Size = new System.Drawing.Size(92, 23);
+            this.DeletehisChatButton.TabIndex = 9;
+            this.DeletehisChatButton.Text = "Delete this chat";
+            this.DeletehisChatButton.UseVisualStyleBackColor = false;
+            this.DeletehisChatButton.Click += new System.EventHandler(this.DeletehisChatButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name";
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.BackColor = System.Drawing.Color.Plum;
+            this.ButtonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.ButtonSend.ForeColor = System.Drawing.Color.Aqua;
+            this.ButtonSend.Location = new System.Drawing.Point(12, 291);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(130, 31);
+            this.ButtonSend.TabIndex = 3;
+            this.ButtonSend.Text = "Send to flight";
+            this.ButtonSend.UseVisualStyleBackColor = false;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(611, 322);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeletehisChatButton);
+            this.Controls.Add(this.CreatePrivatChatButton);
+            this.Controls.Add(this.Name2Box);
+            this.Controls.Add(this.Privat);
+            this.Controls.Add(this.TestBoxS);
+            this.Controls.Add(this.TestBoxL);
+            this.Controls.Add(this.ButtonSend);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.ChatWindow);
+            this.Controls.Add(this.NameBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Swallow Chat";
+            this.Privat.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.RichTextBox ChatWindow;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button ButtonSend;
+        public System.Windows.Forms.RichTextBox TestBoxL;
+        private System.Windows.Forms.RichTextBox TestBoxS;
+        private System.Windows.Forms.GroupBox Privat;
+        private System.Windows.Forms.Button ButtonChat0;
+        private System.Windows.Forms.Button ButtonChat5;
+        private System.Windows.Forms.Button ButtonChat4;
+        private System.Windows.Forms.Button ButtonChat3;
+        private System.Windows.Forms.Button ButtonChat2;
+        private System.Windows.Forms.Button ButtonChat1;
+        private System.Windows.Forms.TextBox Name2Box;
+        private System.Windows.Forms.Button CreatePrivatChatButton;
+        private System.Windows.Forms.Button DeletehisChatButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private string name = "NoName"; //Имя пользователя
+        private string name2 = "";//Имя собеседника
+        private string[][] chatText; // Массив с номером, автором и сообщениями чата
+        private string[][] privatChat; //Массив с именами участников приватных чатов
+        private int lastMessageNumber = 0; //Номер крайней записи
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            DoworkAsync(lastMessageNumber, name, name2, "", 10000);//Вызов Демона Чата
+        }
+
+        private class AnswerServer //Класс получающий ответ сервера
+        {
+            public string[][] chat;
+            public string[][] privat;
+            public string[][] log;
+        }
+
+        private async Task DoworkAsync(int lastMessageNumber, string name, string name2, string inputText, int i) //Асинхронная обертка
+        {
+
+            await Task.Run(() => SendPing(lastMessageNumber, name, name2, inputText, i));
+        }
+
+        private void SendPing(int lastMessageNumber, string name, string name2, string inputText, int i) //Метод-цикл связи
+        {
+            while (i > 0)
+            {
+                lastMessageNumber = this.lastMessageNumber;//Синхронизация полей
+                name = this.name;
+                name2 = this.name2;
+                i--;
+                Send(lastMessageNumber, name, name2, inputText);
+
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void Send(int lastMessageNumber2, string name, string name2, string inputText)//метод связи с сервером
+        {
+         // string url = "http://my1server/index.php";//Адрес хоста
+          // string url = "http://swallowchat.000webhostapp/index.php";//https://swallowchat.000webhostapp.com/
+         string url = "https://swallowchat.000webhostapp.com/";
+            using (var webClient = new WebClient())
+            {
+                var pars = new NameValueCollection();
+                pars.Add("ChatNumber", Convert.ToString(lastMessageNumber2));//Поля отпраки в ПОСТ запрос
+                pars.Add("ChatName", name);
+                pars.Add("ChatName2", name2);
+                pars.Add("ChatInputText", inputText);
+
+                var response = webClient.UploadValues(url, pars); //Метод передачи данных ресурсу
+                string str = Encoding.UTF8.GetString(response);//Раскодировка массива байтов
+
+                AnswerServer answerServer = JsonConvert.DeserializeObject<AnswerServer>(str);//Заполнение экземпляра ответом с сервера
+                chatText = answerServer.chat;//Ссылка на массив сообщений
+                Array.Reverse(chatText);//Пререворачиваем массив
+                privatChat = answerServer.privat;//Ссылка на массив приватных чатов
+
+                if (TestBoxL.InvokeRequired) TestBoxL.Invoke(new Action<string>((s) => TestBoxL.Text = s), str);// Техническая инфа о пришедшем массиве
+                else TestBoxL.Text = str;
+
+                for (int i = 0; i < chatText.Length; i++)//Заполнение окна чата из асинхронного метода или из обычного
+                {
+                    if (chatText[i] == null) break;
+                    if (ChatWindow.InvokeRequired) ChatWindow.Invoke(new Action<string>((s) => ChatWindow.AppendText(s)), (chatText[i][0] + "--" + chatText[i][1] + " : " + chatText[i][2] + " \n"));
+                    else ChatWindow.AppendText((chatText[i][0] + "--" + chatText[i][1] + " : " + chatText[i][2] + " \n"));
+
+                    lastMessageNumber = Convert.ToInt32(chatText[i][0]); //Увеличение счетчика сообщений
+
+                   // if (TestBoxS.InvokeRequired) TestBoxS.Invoke(new Action<string>((s) => TestBoxS.Text = s), Convert.ToString(answerServer.log[0]));
+                   // else TestBoxS.Text = Convert.ToString(answerServer.log[0]);// Техническая инфа о счетчике
+                }
+                ButtonTextRider(privatChat);//Выведение надписей о приватных чатах
+            }
+        }
+        private void ButtonTextRider(string[][] privatChatList) //Метод заполнения надписей на кнопках приватных чатов
+        {
+
+            string nameButton;
+            Button button = new Button();
+
+            for (int i = 0; i < 6; i++)//Перебор массива приватных чатов
+            {
+                if (privatChatList.Length > i) { nameButton = privatChatList[i][1] + " ---->   " + privatChatList[i][2]; }
+                else nameButton = "";
+
+
+                if (nameButton != null) //Выбор кнопок по именам
+                {
+                    button = Privat.Controls["ButtonChat" + i.ToString()] as Button;
+
+                    if (button.InvokeRequired) button.Invoke(new Action<string>((s) => button.Text = s), nameButton);
+                    else button.Text = nameButton;
+                }
+            }
+        }
+
+        private void NameBox_KeyPress(object sender, KeyPressEventArgs e)//Ввод имени по нажатию Энтер
+        {
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    name = Convert.ToString(NameBox.Text);
+                    name2 = "";
+                    ButtonChatGray(7);
+                }
+
+            }
+        }
+
+        private void ChatWindow_TextChanged(object sender, EventArgs e)//Смещение текста чата вверх
+        {
+            ChatWindow.SelectionStart = ChatWindow.Text.Length;//Задает начальную позицию текста
+            ChatWindow.ScrollToCaret();//Сдвигает каретку вниз
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)//Отправка сообщения
+        {
+            if (name != "NoName")
+            {
+                string text = InputBox.Text;
+                DoworkAsync(lastMessageNumber, name, name2, text, 1);
+                InputBox.Text = "";
+            }
+
+        }
+
+        private void InputBox_KeyPress(object sender, KeyPressEventArgs e)//Отправка сообщения по нажатию Энтер
+        {
+            if (name != "NoName")
+            {
+                if (Char.IsControl(e.KeyChar))
+                {
+                    if (e.KeyChar == (char)Keys.Enter)
+                    {
+                        string text = InputBox.Text;
+                        DoworkAsync(lastMessageNumber, name, name2, text, 1);
+                        InputBox.Text = "";
+                    }
+                }
+            }
+        }
+
+        //Нажатие на кнопки приватных чатов
+        private void ButtonChat0_Click(object sender, EventArgs e)
+        {
+            ButtonPrivatChatClick(0, privatChat);
+        }
+        private void ButtonChat1_Click(object sender, EventArgs e)
+        {
+            ButtonPrivatChatClick(1, privatChat);
+        }
+        private void ButtonChat2_Click(object sender, EventArgs e)
+        {
+
+            ButtonPrivatChatClick(2, privatChat);
+        }
+        private void ButtonChat3_Click(object sender, EventArgs e)
+        {
+            ButtonPrivatChatClick(3, privatChat);
+        }
+        private void ButtonChat4_Click(object sender, EventArgs e)
+        {
+            ButtonPrivatChatClick(4, privatChat);
+        }
+        private void ButtonChat5_Click(object sender, EventArgs e)
+        {
+            ButtonPrivatChatClick(5, privatChat);
+        }
+
+        private void ButtonPrivatChatClick(int numberButton, string[][] privatChatList) //Метод при нажатии Кнопки приватного  чата
+        {
+            string nameButton;
+            Button button = new Button();
+            ChatWindow.Text = "";
+            lastMessageNumber = 0;
+
+            for (int i = 0; i < privatChatList.Length; i++)//Перебор массива приватных чатов
+            { 
+                button = Privat.Controls["ButtonChat" + i.ToString()] as Button;//Присваивание кнопки по имени
+
+                //Присваивание имени собеседнику
+                if (i == numberButton && button.BackColor == SystemColors.Control) { name2 = privatChatList[i][1]; }
+                if (name2 == name) name2 = privatChatList[i][2];
+                if (i == numberButton && button.BackColor == Color.Green) { name2 = ""; }
+
+
+                //Определение будущего имени кнопки
+                if (privatChatList.Length > i) { nameButton = privatChatList[i][1] + " >>>>>>>>   " + privatChatList[i][2]; }
+                else nameButton = "";
+
+
+                if (nameButton != null) //Выбор кнопок по именам
+                {
+                    if (button.InvokeRequired) button.Invoke(new Action<string>((s) => button.Text = s), nameButton);
+                    else button.Text = nameButton;
+                }
+            }
+
+            //Перекраска кнопок
+            ButtonChatGray(numberButton);
+        }
+
+        private void ButtonChatGray(int greenButton)//Перекраска кнопок чатов в серый цвет
+        {   
+                Button button = new Button();
+
+            for (int i = 0; i < 6; i++)
+            {
+                button = Privat.Controls["ButtonChat" + i.ToString()] as Button;
+
+                if (button.InvokeRequired && i == greenButton && name2!="" ) button.Invoke(new Action<Color>((s) => button.BackColor = s), Color.Green);
+                else if (i == greenButton && name2 != "") button.BackColor = Color.Green;
+                else if (button.InvokeRequired ) { button.Invoke(new Action<Color>((s) => button.BackColor = s), SystemColors.Control); }
+                else  { button.BackColor = SystemColors.Control; }  
+            }
+        }
+
+        private void CreatePrivatChatButton_Click(object sender, EventArgs e)
+        {
+            name2 = Name2Box.Text;
+            Send(-1, name, name2, "");
+            ChatWindow.Text = "";
+            lastMessageNumber = 0;
+            ButtonChatGray(privatChat.Length-1);
+        }//Создание приватного чата
+
+        private void DeletehisChatButton_Click(object sender, EventArgs e)//Удаление чата
+        {
+            Send(-2, name, name2, "");
+            if (name2 != "") { ChatWindow.Text += "\n Удалён приватный чат с" + name2; }
+            lastMessageNumber = 0;
+            name2 = "";
+            ButtonChatGray(7);
+        }
+
+        //Удаление серой подсказки
+        private void InputBox_Enter(object sender, EventArgs e)
+        {
+            if (InputBox.ForeColor == Color.Gray)
+            {
+                InputBox.Text = "";
+                InputBox.ForeColor = Color.Black;
+            }
+        }
+        private void NameBox_Enter(object sender, EventArgs e)
+        {
+            if (NameBox.ForeColor == Color.Gray)
+            { 
+                NameBox.Text = "";
+                NameBox.ForeColor = Color.Black;
+            }
+            if (NameBox.ForeColor == Color.Red) NameBox.ForeColor = Color.Gray;
+        }
+    }
+}
